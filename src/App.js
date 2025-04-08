ad6281314c6b5b062e7a855e547da7ed2e6271a0
Sardar Noman Shakir <121171686+CodeWithZavi@users.noreply.github.com>
2025-04-08 16:02:03 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
-import { act } from '@testing-library/react';
+
 
 // Pages
 import Home from './pages/Home';
